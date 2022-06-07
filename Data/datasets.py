9a96629e63c1be06c6caa73f8766f91ddad2f19a
NaN <43895813+tmartin2@users.noreply.github.com>
2022-06-07 09:04:37 -0400

    Add files via upload

--- Data/datasets.py
+++ Data/datasets.py
@@ -6,270 +6,66 @@ import numpy as np
 import pandas as pd
 from random import sample, seed
 
+# this is the default random state across all files
+RANDOM_STATE = 123432
 
-def read_seq_data(
-  file,
-  dataset,
-  number=0
-  ):
-  """
-  Parameters
-  ----------
-  file: a string defining the path to a particular data file
-  dataset: a string {nn269, ce, hs3d} indicating which dataset to use
-  number: integer, delimit special cases, such as files with headers
 
-  Returns
-  -------
-  seq_data: a list of nucleotide sequence strings
-  """
-  # NN269
-  if dataset == 'nn269':
-    with open(file, 'r') as f:
-      all_lines = f.readlines()
-      seq_data = [elt.replace('\n','').replace(' ','') for elt in list(filter(lambda line: False if '>' in line else True, all_lines))]
-      seq_data = list(filter(None, seq_data))
-      f.close()
-    return seq_data
-  # HS3D subset
-  if dataset == 'hs3d':
-    with open(file, 'r') as f:
-      all_lines = f.readlines()
-      seq_data = [elt.replace('\n','').replace(' ','') for elt in list(filter(lambda line: False if '>' in line else True, all_lines))]
-      seq_data = list(filter(None, seq_data))
-      f.close()
-    return seq_data
-  # Splice2Deep data
-  if dataset in ['hs2','ce2','dm','oy','ar']:
-    with open(file, 'r') as f:
-      all_lines = f.readlines()
-      seq_data = [elt.replace('\n','').replace(' ','') for elt in list(filter(lambda line: False if '>' in line else True, all_lines))]
-      seq_data = list(filter(None, seq_data))
-      f.close()
-    return seq_data
+# there are idiosyncracies (mostly tags) to each dataset that must be dealt with
+def read_seq_data(file):
+  with open(file, 'r') as f:
+    all_lines = f.readlines()
+    seq_data = [elt.replace('\n','').replace(' ','') for elt in list(filter(lambda line: False if '>' in line else True, all_lines))]
+    seq_data = list(filter(None, seq_data))
+    f.close()
+  return seq_data
 
-# ./Datasets/ for local machine
-def read_nn269(kwargs):
-  dataset = 'nn269'
-  # get training data
-  if kwargs['train'] or kwargs['validate']:
-    atn_x = read_seq_data('./Datasets/NN269/Train/Acceptor_Train_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/NN269/Train/Acceptor_Train_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/NN269/Train/Donor_Train_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/NN269/Train/Donor_Train_Positive.txt', dataset)
-  # get testing data
-  if kwargs['test']:
-    atn_x = read_seq_data('./Datasets/NN269/Test/Acceptor_Test_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/NN269/Test/Acceptor_Test_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/NN269/Test/Donor_Test_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/NN269/Test/Donor_Test_Positive.txt', dataset)
-  # balance the dataset
-  if kwargs['balance']:
-    if (len(atn_x) >= len(atp_x)):
-      seed(kwargs['state'])
-      atn_x = sample(atn_x, len(atp_x))
-    if (len(atn_x) <= len(atp_x)):
-      seed(kwargs['state'])
-      atp_x = sample(atp_x, len(atn_x))
-    if (len(dtn_x) >= len(dtp_x)):
-      seed(kwargs['state'])
-      dtn_x = sample(dtn_x, len(dtp_x))
-    if (len(dtn_x) <= len(dtp_x)):
-      seed(kwargs['state'])
-      dtp_x = sample(dtp_x, len(dtn_x))
-  # acceptor and donor labels
+# read each dataset (call its specific function)
+def read_dataset(dataset, kwargs):
+
+  # CHANGE FROM PLAYDATA to Datasets
+  # RENAME ALL Subtrain to Train, same for test
+  # RENAME Acceptor_Train_Negative to Acceptor_Train_Negative
+  # if training, use the training files, otherwise use testing files
+  if (kwargs["operation"] == 'train') or (kwargs["operation"] == 'validate') or (kwargs["operation"] == 'tune'):
+    atn_x = read_seq_data(f'./ENSDatasets/{dataset}/Train/Acceptor_Train_Negative.txt')
+    atp_x = read_seq_data(f'./ENSDatasets/{dataset}/Train/Acceptor_Train_Positive.txt')
+    dtn_x = read_seq_data(f'./ENSDatasets/{dataset}/Train/Donor_Train_Negative.txt')
+    dtp_x = read_seq_data(f'./ENSDatasets/{dataset}/Train/Donor_Train_Positive.txt')
+  if (kwargs["operation"] == 'test'):
+    atn_x = read_seq_data(f'./ENSDatasets/{dataset}/Test/Acceptor_Test_Negative.txt')
+    atp_x = read_seq_data(f'./ENSDatasets/{dataset}/Test/Acceptor_Test_Positive.txt')
+    dtn_x = read_seq_data(f'./ENSDatasets/{dataset}/Test/Donor_Test_Negative.txt')
+    dtp_x = read_seq_data(f'./ENSDatasets/{dataset}/Test/Donor_Test_Positive.txt')
+
+  # acceptor and donor labels, positive is 1 negative is 0
   atp_y = [1]*len(atp_x)
   atn_y = [0]*len(atn_x)
   dtp_y = [1]*len(dtp_x)
   dtn_y = [0]*len(dtn_x)
-  # acceptor and donor dataset
+
+  # acceptor and donor datasets (concatenate pos, neg values and pos, neg labels)
   acc_x, acc_y  = atn_x+atp_x, atn_y+atp_y
   don_x, don_y  = dtn_x+dtp_x, dtn_y+dtp_y
   acc = {'Acceptor Sites':acc_x, 'Acceptor Labels':acc_y}
   don = {'Donor Sites':don_x, 'Donor Labels':don_y}
   acc_df = pd.DataFrame(data=acc)
   don_df = pd.DataFrame(data=don)
-  # shuffle datasets
-  acc_df, don_df = acc_df.sample(frac=1, random_state=kwargs['state']), don_df.sample(frac=1, random_state=kwargs['state'])
-  return (acc_df, don_df)
 
-# currently for subset of hs3d
-def read_hs3d(kwargs):
-  dataset = 'hs3d'
-  # get training data
-  if kwargs['train'] or kwargs['validate']:
-    atn_x = read_seq_data('./Datasets/HS3D/SubsetTrain/Acceptor_Train_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/HS3D/SubsetTrain/Acceptor_Train_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/HS3D/SubsetTrain/Donor_Train_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/HS3D/SubsetTrain/Donor_Train_Positive.txt', dataset)
-  # get testing data
-  if kwargs['test']:
-    atn_x = read_seq_data('./Datasets/HS3D/SubsetTest/Acceptor_Test_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/HS3D/SubsetTest/Acceptor_Test_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/HS3D/SubsetTest/Donor_Test_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/HS3D/SubsetTest/Donor_Test_Positive.txt', dataset)
-  # balance the dataset
-  if kwargs['balance']:
-    if (len(atn_x) >= len(atp_x)):
-      seed(kwargs['state'])
-      atn_x = sample(atn_x, len(atp_x))
-    if (len(atn_x) <= len(atp_x)):
-      seed(kwargs['state'])
-      atp_x = sample(atp_x, len(atn_x))
-    if (len(dtn_x) >= len(dtp_x)):
-      seed(kwargs['state'])
-      dtn_x = sample(dtn_x, len(dtp_x))
-    if (len(dtn_x) <= len(dtp_x)):
-      seed(kwargs['state'])
-      dtp_x = sample(dtp_x, len(dtn_x))
-
-  # acceptor and donor labels
-  atp_y = [1]*len(atp_x)
-  atn_y = [0]*len(atn_x)
-  dtp_y = [1]*len(dtp_x)
-  dtn_y = [0]*len(dtn_x)
-  # acceptor and donor dataset
-  acc_x, acc_y  = atn_x+atp_x, atn_y+atp_y
-  don_x, don_y  = dtn_x+dtp_x, dtn_y+dtp_y
-  acc = {'Acceptor Sites':acc_x, 'Acceptor Labels':acc_y}
-  don = {'Donor Sites':don_x, 'Donor Labels':don_y}
-  acc_df = pd.DataFrame(data=acc)
-  don_df = pd.DataFrame(data=don)
   # shuffle datasets
-  acc_df, don_df = acc_df.sample(frac=1, random_state=kwargs['state']), don_df.sample(frac=1, random_state=kwargs['state'])
+  acc_df, don_df = acc_df.sample(frac=1, random_state=RANDOM_STATE), don_df.sample(frac=1, random_state=RANDOM_STATE)
+
+  # return the acceptor and donor dataframes
   return (acc_df, don_df)
 
 
-def read_ar(kwargs):
-  dataset = 'ar' # multiple will have the same effect
-  # get training data
-  if kwargs['train'] or kwargs['validate']:
-    atn_x = read_seq_data('./Datasets/Arabidopsis/SubsetTrain/Acceptor_Train_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/Arabidopsis/SubsetTrain/Acceptor_Train_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/Arabidopsis/SubsetTrain/Donor_Train_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/Arabidopsis/SubsetTrain/Donor_Train_Positive.txt', dataset)
-  # get testing data
-  if kwargs['test']:
-    atn_x = read_seq_data('./Datasets/Arabidopsis/SubsetTest/Acceptor_Test_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/Arabidopsis/SubsetTest/Acceptor_Test_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/Arabidopsis/SubsetTest/Donor_Test_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/Arabidopsis/SubsetTest/Donor_Test_Positive.txt', dataset)
-  # balance the dataset
-  if kwargs['balance']:
-    if (len(atn_x) >= len(atp_x)):
-      seed(kwargs['state'])
-      atn_x = sample(atn_x, len(atp_x))
-    if (len(atn_x) <= len(atp_x)):
-      seed(kwargs['state'])
-      atp_x = sample(atp_x, len(atn_x))
-    if (len(dtn_x) >= len(dtp_x)):
-      seed(kwargs['state'])
-      dtn_x = sample(dtn_x, len(dtp_x))
-    if (len(dtn_x) <= len(dtp_x)):
-      seed(kwargs['state'])
-      dtp_x = sample(dtp_x, len(dtn_x))
+# recall, these are the kwargs
 
-  # acceptor and donor labels
-  atp_y = [1]*len(atp_x)
-  atn_y = [0]*len(atn_x)
-  dtp_y = [1]*len(dtp_x)
-  dtn_y = [0]*len(dtn_x)
-  # acceptor and donor dataset
-  acc_x, acc_y  = atn_x+atp_x, atn_y+atp_y
-  don_x, don_y  = dtn_x+dtp_x, dtn_y+dtp_y
-  acc = {'Acceptor Sites':acc_x, 'Acceptor Labels':acc_y}
-  don = {'Donor Sites':don_x, 'Donor Labels':don_y}
-  acc_df = pd.DataFrame(data=acc)
-  don_df = pd.DataFrame(data=don)
-  # shuffle datasets
-  acc_df, don_df = acc_df.sample(frac=1, random_state=kwargs['state']), don_df.sample(frac=1, random_state=kwargs['state'])
-  return (acc_df, don_df)
-
-def read_hs2(kwargs):
-  dataset = 'hs2' # multiple will have the same effect
-  # get training data
-  if kwargs['train'] or kwargs['validate']:
-    atn_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTrain/Acceptor_Train_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTrain/Acceptor_Train_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTrain/Donor_Train_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTrain/Donor_Train_Positive.txt', dataset)
-  # get testing data
-  if kwargs['test']:
-    atn_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTest/Acceptor_Test_Negative.txt', dataset)
-    atp_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTest/Acceptor_Test_Positive.txt', dataset)
-    dtn_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTest/Donor_Test_Negative.txt', dataset)
-    dtp_x = read_seq_data('./Datasets/Homo_sapiens/SubsetTest/Donor_Test_Positive.txt', dataset)
-  # balance the dataset
-  if kwargs['balance']:
-    if (len(atn_x) >= len(atp_x)):
-      seed(kwargs['state'])
-      atn_x = sample(atn_x, len(atp_x))
-    if (len(atn_x) <= len(atp_x)):
-      seed(kwargs['state'])
-      atp_x = sample(atp_x, len(atn_x))
-    if (len(dtn_x) >= len(dtp_x)):
-      seed(kwargs['state'])
-      dtn_x = sample(dtn_x, len(dtp_x))
-    if (len(dtn_x) <= len(dtp_x)):
-      seed(kwargs['state'])
-      dtp_x = sample(dtp_x, len(dtn_x))
-
-  # acceptor and donor labels
-  atp_y = [1]*len(atp_x)
-  atn_y = [0]*len(atn_x)
-  dtp_y = [1]*len(dtp_x)
-  dtn_y = [0]*len(dtn_x)
-  # acceptor and donor dataset
-  acc_x, acc_y  = atn_x+atp_x, atn_y+atp_y
-  don_x, don_y  = dtn_x+dtp_x, dtn_y+dtp_y
-  acc = {'Acceptor Sites':acc_x, 'Acceptor Labels':acc_y}
-  don = {'Donor Sites':don_x, 'Donor Labels':don_y}
-  acc_df = pd.DataFrame(data=acc)
-  don_df = pd.DataFrame(data=don)
-  # shuffle datasets
-  acc_df, don_df = acc_df.sample(frac=1, random_state=kwargs['state']), don_df.sample(frac=1, random_state=kwargs['state'])
-  return (acc_df, don_df)
-
-
-def read_dataset(
-  dataset,
-  kwargs,
-  ):
-  """
-  Parameters
-  ----------
-  dataset: a string {nn269, ce, hs3d, } indicating which dataset to use
-  kwargs: dictionary, arguments found in the exec file
-
-  Returns
-  -------
-  (acc_df, don_df) : a tuple of pandas dataframes, one per splice site type
-  """
-
-  # READ NN269
-  if dataset == 'nn269':
-    return read_nn269(kwargs)
-
-  # READ NN269
-  if dataset == 'hs3d':
-    return read_hs3d(kwargs)
-
-  # READ HS2
-  if dataset == 'hs2':
-    return read_hs2(kwargs)
-
-  # READ AR
-  if dataset == 'ar':
-    return read_ar(kwargs)
-
-  # COMBINE DATASET
-  if dataset == 'combine':
-    ar = read_ar(kwargs)
-    hs2 = read_hs2(kwargs)
-    acc_dfs = [ar[0], hs2[0]]
-    don_dfs = [ar[1], hs2[1]]
-    acc_comb = pd.concat(acc_dfs, sort=False, ignore_index=True)
-    don_comb = pd.concat(don_dfs, sort=False, ignore_index=True)
-    # shuffle
-    acc_df, don_df = acc_comb.sample(frac=1, random_state=kwargs['state']), don_comb.sample(frac=1, random_state=kwargs['state'])
-    return (acc_df, don_df)
+# ensemblesplice.run(
+  #     ensemble=args.esplice,
+  #     sub_models=sub_models,
+  #     datasets=datasets,
+  #     splice_sites=splice_sites,
+  #     operation=operation[0][1],
+  #     folds=args.k,
+  #     report=args.report,
+  # )
--- Data/encode_data.py
+++ Data/encode_data.py
@@ -7,23 +7,8 @@ from Data import datasets
 import tensorflow as tf
 
 
-def encode(
-    dataset,
-    model,
-    kwargs
-    ):
-    """
-    Parameters
-    ----------
-    dataset: a string {nn269, ce, hs3d} indicating which dataset to use
-    model_names: the names of different models used
-    kwargs:
+def encode(dataset, ss_type, kwargs):
 
-    Returns
-    -------
-    [acc_x, acc_y, don_x, don_y]: list, of pandas dataframe containing
-        true and false acceptor and donor sites and their labels
-    """
     # acquire shuffled datasets
     acc_df, don_df = datasets.read_dataset(dataset, kwargs)
 
@@ -51,25 +36,14 @@ def encode(
         dtype='float32'
     )
 
-    # Data for CONV2D layers
-    if ('cnn1' == model) or ('cnn5' == model):
-        # create new dataset if cnn1 or cnn5 is used.
-        conv2d_acc_df, conv2d_don_df = acc_df.copy(deep=True), don_df.copy(deep=True)
-        # reshape each entry from (rows,cols) to (1, rows, cols, 1) for use with Conv2D
-        expand_2 = lambda seq_mat: seq_mat.reshape(1, seq_mat.shape[0], seq_mat.shape[1], -1)
-        conv2d_acc_df['Acceptor Sites'] = conv2d_acc_df['Acceptor Sites'].apply(expand_2)
-        conv2d_don_df['Donor Sites'] = conv2d_don_df['Donor Sites'].apply(expand_2)
-        # make dataset of shape (len(data), rows, cols, 1) by vertically stacking data
-        conv2d_acc_x = np.vstack(conv2d_acc_df['Acceptor Sites'].to_numpy())
-        conv2d_don_x = np.vstack(conv2d_don_df['Donor Sites'].to_numpy())
-        return {'acceptor':(conv2d_acc_x, acc_y), 'donor':(conv2d_don_x, don_y)}
-    else:
-        # Data for CONV1D, LTSM, Dense
-        # reshapes each entry from (rows, cols) to (1, rows, cols) for use with LTSM, DNN, CONV1D
-        expand_1 = lambda seq_mat: seq_mat.reshape(1, seq_mat.shape[0], seq_mat.shape[1])
-        acc_df['Acceptor Sites'] = acc_df['Acceptor Sites'].apply(expand_1)
-        don_df['Donor Sites'] = don_df['Donor Sites'].apply(expand_1)
-        # make dataset of shape (len(data), rows, cols) by vertically stacking data
-        acc_x = np.vstack(acc_df['Acceptor Sites'].to_numpy())
-        don_x = np.vstack(don_df['Donor Sites'].to_numpy())
-        return {'acceptor':(acc_x, acc_y), 'donor':(don_x, don_y)}
+    # reshapes each entry from (rows, cols) to (1, rows, cols) for use with LTSM, DNN, CONV1D
+    expand_1 = lambda seq_mat: seq_mat.reshape(1, seq_mat.shape[0], seq_mat.shape[1])
+    acc_df['Acceptor Sites'] = acc_df['Acceptor Sites'].apply(expand_1)
+    don_df['Donor Sites'] = don_df['Donor Sites'].apply(expand_1)
+    # make dataset of shape (len(data), rows, cols) by vertically stacking data
+    acc_x = np.vstack(acc_df['Acceptor Sites'].to_numpy())
+    don_x = np.vstack(don_df['Donor Sites'].to_numpy())
+    if ss_type == "acceptor":
+        return (acc_x, acc_y)
+    if ss_type == "donor":
+        return (don_x, don_y)
